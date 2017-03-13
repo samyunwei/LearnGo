7f3e39fce617a9099e5af50634a4def3cc9e3bd9
Sam <259375702@qq.com>
2017-03-14 00:54:17 +0800

    add fmt

--- src/stringapi/stringapi.go
+++ src/stringapi/stringapi.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"unicode"
 	"unicode/utf8"
+	"math"
 )
 
 func stringoperators() {
@@ -52,10 +53,89 @@ func StringIndex2() {
 	fmt.Println(firstword, lastword)
 }
 
+func StringFmtPrint() {
+	type polar struct {
+		radius, o float64
+	}
+	p := polar{8.32, .49}
+	fmt.Print(-18.5, 17, "Elephant", -8+.7i, 0x3c7, '\u03C7', "a", "b", p)
+	fmt.Println()
+	fmt.Println(-18.5, 17, "Elephant", -8+.7i, 0x3c7, '\u03C7', "a", "b", p)
+
+}
+
+func IntForBool(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+func StringFmtBool() {
+	fmt.Printf("%t %t\n", true, false)
+	fmt.Printf("%d %d\n", IntForBool(true), IntForBool(false))
+}
+func Pad(number, width int, pad rune) string {
+	s := fmt.Sprint(number)
+	gap := width - utf8.RuneCountInString(s)
+	if gap > 0 {
+		return strings.Repeat(string(pad), gap) + s
+	}
+	return s
+}
+func StringFmtInt() {
+	fmt.Printf("|%b|%9b|%-9b|%09b|% 09b|\n", 37, 37, 37, 37, 37)
+	fmt.Printf("|%o|%#o|%# 8o|%#+ 8o|%+08o|\n", 41, 41, 41, 41, -41)
+	i := 3931
+	fmt.Printf("|%x|%X|%8x|%08x|%#04X|0x%04X|\n", i, i, i, i, i, i)
+	i = 569
+	fmt.Printf("|$%d|$%06d|$%+06d|$%s|\n", i, i, i, Pad(i, 6, '*'))
+}
+
+func StringFmtString() {
+	fmt.Printf("%d %#04x %U '%c' \n", 0x3A6, 934, '\u03A6', '\U000003A6')
+}
+
+func Humanize(amount float64, width, decimals int, pad, separator rune) string {
+	dollars, cents := math.Modf(amount)
+	whole := fmt.Sprintf("%+.0f", dollars)[1:]
+	fraction := ""
+	if decimals > 0 {
+		fraction = fmt.Sprintf("%+.*f", decimals, cents)[2:]
+	}
+	sep := string(separator)
+	for i := len(whole) - 3; i > 0; i -= 3 {
+		whole = whole[:i] + sep + whole[i:]
+	}
+	if amount < 0.0 {
+		whole = "-" + whole
+	}
+	number := whole + fraction
+	gap := width - utf8.RuneCountInString(number)
+	if gap > 0 {
+		return strings.Repeat(string(pad), gap) + number
+	}
+	return number
+}
+
+func stringFmtFloat() {
+	for _, x := range [] float64{-.258, 7194.84, -60897162.0218, 1.500089e-8} {
+		fmt.Printf("|%20.5e|%20.5f|%s|\n", x, x, Humanize(x, 20, 5, '*', ','))
+	}
+
+	for _, x := range []complex128{2 + 3i, 172.6 - 58.3019i, -.827e2 + 9.04831e-3i} {
+		fmt.Printf("|%15s|%9.3f|%.2f|%.1e|\n", fmt.Sprintf("%6.2f%+.3fi", real(x), imag(x)), x, x, x)
+	}
+
+}
 func main() {
 	//stringoperators()
 	//charAndString()
 	//RuneandIndex()
 	//StringIndex()
-	StringIndex2()
+	//StringIndex2()
+	//StringFmtPrint()
+	//StringFmtBool()
+	//StringFmtInt()
+	//StringFmtString()
+	stringFmtFloat()
 }
